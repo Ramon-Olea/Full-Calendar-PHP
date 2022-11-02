bd2a419590c69263478116eaa0b121c1949eeefb
Ramon Olea <50149082+Ramon-Olea@users.noreply.github.com>
2022-11-02 17:50:04 -0600

    FULL CALENDAR

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "http://192.168.31.46/eventos/";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "eventos";
+const charset = "charset=utf8";
+?>
